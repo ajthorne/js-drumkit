d39d29c8c7ce4ca5156c42a06e5ac96bf20eda40
Amanda Thorne <manda.thorne@gmail.com>
2016-12-14 15:04:53 -0600

    some stylings

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 window.addEventListener('keydown', function(e) {
   // console.log(e);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  // console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add('playing');
+  // console.log(key);
   if (!audio) {
     return;
   }
+  audio.currentTime = 0;
   audio.play();
 });
+
+// function removeTransition(e) {
+  // console.log(e);
+// }
+
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition))
